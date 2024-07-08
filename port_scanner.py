268e500b90d2ae91af077690c414406516f77e49
João Vítor <joao.paes@philips.com>
2024-07-08 01:20:37 +0000

    first changes on main structure

--- port_scanner.py
+++ port_scanner.py
@@ -3,6 +3,14 @@ import socket
 def get_open_ports(target, port_range, verbose = False):
     open_ports = []
 
-
+    if verbose:
+        ip_address = '192.168.0.10'
+        open_ports = f'Open ports for {target} ({ip_address})\nPORT\tSERVICE\n'; 
+        
+    else: 
+        open_ports.append(1)
 
     return(open_ports)
+
+ports = get_open_ports("www.freecodecamp.org", [75, 85], True)
+print("Open ports:", ports, "\n")
